17223c25b08752b8400cde580de7bf8ad9d5e4fa
jwbats <jwbats@gmail.com>
2015-06-26 22:33:25 +0200

    Brought the directive controller out of the directive.

---
+++ Sandbox/js/controllers/personListing.js
@@ -0,0 +1,18 @@
+﻿(function(){
+
+	angular.module("SandboxApp").controller("PersonListingCntrl", function($scope){
+		$scope.title = "Person Listing";
+				
+		$scope.persons = [
+			{ FirstName: "Joe",   LastName: "Joeington" },
+			{ FirstName: "Eddy",  LastName: "Edzma" },
+			{ FirstName: "Larry", LastName: "Laffer" },
+			{ FirstName: "Jay",   LastName: "Bats" }
+		];
+
+		$scope.fullname = function(person){
+			return person.FirstName + " " + person.LastName;
+		};
+	});
+
+})();
---
+++ Sandbox/js/directives/personListing.js
@@ -0,0 +1,12 @@
+﻿(function(){
+
+	angular.module("person-directives", []).directive("personListing", function(){
+		return {
+			restrict: "E",
+			templateUrl: "../personListing.html",
+			scope: {},
+			controller: "PersonListingCntrl"
+		};	
+	});
+
+})();
