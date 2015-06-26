16171083e4a0a97ee16e99b9325109db93e2bfad
jwbats <jwbats@gmail.com>
2015-06-26 16:43:09 +0200

    Initial commit.

---
+++ angularJS/Default.aspx
@@ -0,0 +1,129 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+	<style>
+		table, th, td {
+			border: 1px solid grey;
+			border-collapse: collapse;
+			padding: 5px;
+		}
+
+			table th {
+				background-color: #274257;
+				color: #fff;
+			}
+
+			table tr:nth-child(odd) {
+				background-color: #f1f1f1;
+			}
+
+			table tr:nth-child(even) {
+				background-color: #ffffff;
+			}
+	</style>
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<div ng-app="myApp" ng-controller="myCntrl">
+			<table>
+				<tr>
+					<td>Student Name :
+					</td>
+					<td>
+						<input type="text" id="txtStudentName" ng-model="studentName" />
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" align="center">
+						<input type="button" value="Save" ng-click="Save()" />
+					</td>
+				</tr>
+			</table>
+			<br />
+			<br />
+			<table>
+				<thead>
+					<tr>
+						<th>StudentID
+						</th>
+						<th>StudentName
+						</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tr ng-repeat="student in StudentList | orderBy : studentorder ">
+					<td ng-bind="student.StudentID"></td>
+					<td ng-bind="student.StudentName"></td>
+					<td>
+						<a href="#" ng-click="Delete(student.StudentID)">Delete</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<script>
+			var app = angular.module("myApp", []);
+
+			app.controller("myCntrl", function ($scope, $http) {
+				$scope.studentorder = "StudentID";
+				$scope.studentName = "";
+
+				$scope.Save = function () {
+					var httpreq = {
+						method: 'POST',
+						url: 'Default.aspx/Save',
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'dataType': 'json'
+						},
+						data: { StudentName: $scope.studentName }
+					}
+					$http(httpreq).success(function (response) {
+						$scope.fillList();
+						alert("Saved successfully.");
+					})
+				};
+
+				$scope.Delete = function (SID) {
+					if (confirm("Are you sure want to delete?")) {
+						var httpreq = {
+							method: 'POST',
+							url: 'Default.aspx/Delete',
+							headers: {
+								'Content-Type': 'application/json; charset=utf-8',
+								'dataType': 'json'
+							},
+							data: { StudentID: SID }
+						}
+						$http(httpreq).success(function (response) {
+							$scope.fillList();
+							alert("Deleted successfully.");
+						})
+					}
+				};
+
+				$scope.fillList = function () {
+					$scope.studentName = "";
+					var httpreq = {
+						method: 'POST',
+						url: 'Default.aspx/GetList',
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+							'dataType': 'json'
+						},
+						data: {}
+					}
+					$http(httpreq).success(function (response) {
+						$scope.StudentList = response.d;
+					})
+				};
+
+				$scope.fillList();
+			});
+		</script>
+	</form>
+</body>
+</html>
---
+++ angularJS/Default.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+
+	#region ================================================== Private Members ==================================================
+
+	private static string Connection
+	{
+		get
+		{
+			return ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+		}
+	}
+
+	#endregion ================================================== Private Members ==================================================
+
+
+
+
+
+	#region ================================================== Events ==================================================
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+	}
+
+	#endregion ================================================== Events ==================================================
+
+
+
+
+	#region ================================================== Public Methods ==================================================
+
+	[System.Web.Services.WebMethod()]
+	public static void Save(string StudentName)
+	{
+		using (SqlConnection con = new SqlConnection(Connection))
+		{
+			using (SqlCommand cmd = new SqlCommand())
+			{
+				cmd.Connection = con;
+				cmd.CommandText = "insert into tblStudents (StudentName) values (@StudentName);";
+				cmd.Parameters.AddWithValue("@StudentName", StudentName);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+			}
+		}
+	}
+
+
+
+
+	[System.Web.Services.WebMethod()]
+	public static void Delete(int StudentID)
+	{
+		using (SqlConnection con = new SqlConnection(Connection))
+		{
+			using (SqlCommand cmd = new SqlCommand())
+			{
+				cmd.Connection = con;
+				cmd.CommandText = "update tblStudents set IsActive=0 where StudentID=@StudentID;";
+				cmd.Parameters.AddWithValue("@StudentID", StudentID);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+			}
+		}
+	}
+
+
+
+
+	[System.Web.Services.WebMethod()]
+	public static List<Names> GetList()
+	{
+		List<Names> names = new List<Names>();
+		DataSet ds = new DataSet();
+		using (SqlConnection con = new SqlConnection(Connection))
+		{
+			con.Open();
+
+			using (SqlCommand cmd = new SqlCommand())
+			{
+				cmd.Connection = con;
+				cmd.CommandText = "select StudentID,StudentName from tblStudents where IsActive=1;";
+				using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+				{
+					da.Fill(ds);
+				}
+			}
+		}
+		if (ds != null && ds.Tables.Count > 0)
+		{
+			foreach (DataRow dr in ds.Tables[0].Rows)
+				names.Add(new Names(int.Parse(dr["StudentID"].ToString()), dr["StudentName"].ToString()));
+		}
+		return names;
+	}
+
+	#endregion ================================================== Public Methods ==================================================
+
+}
+
+public class Names
+{
+	public int StudentID;
+	public string StudentName;
+	public Names(int _StudentID, string _StudentName)
+	{
+		StudentID = _StudentID;
+		StudentName = _StudentName;
+	}
+}
