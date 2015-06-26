d5727c76ed404c50e7aa389c2985856342c95979
jwbats <jwbats@gmail.com>
2015-06-26 21:21:47 +0200

    Initial commit.

---
+++ Sandbox/js/app.js
@@ -0,0 +1,38 @@
+﻿(function(){
+
+	var app = angular.module("SandboxApp", []);
+
+	app.controller("MathCntrl", function($scope){
+		$scope.A = 5;
+		$scope.B = 10;
+
+		$scope.mul = function(){
+			return this.A * this.B;
+		};
+
+		$scope.div = function(){
+			return this.A / this.B;
+		};
+
+		$scope.add = function(){
+			return parseInt(this.A) + parseInt(this.B);
+		};
+
+		$scope.sub = function(){
+			return this.A - this.B;
+		};
+	});
+
+	app.controller("StringCntrl", function($scope){
+		$scope.str = "I am a string.";
+
+		$scope.toUpper = function(){
+			return this.str.toUpperCase();
+		};
+
+		$scope.toLower = function(){
+			return this.str.toLowerCase();
+		};
+	});
+
+})();
