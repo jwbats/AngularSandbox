0390799334410e3161a5172b6a67587eaffe9882
jwbats <jwbats@gmail.com>
2015-07-02 13:41:18 +0200

    Modifications...

--- Stuff/AngularBindings/script.js
+++ Stuff/AngularBindings/script.js
@@ -1,27 +1,12 @@
 angular.module("isolate", [])
-    .directive("myDirective", function(){
-        return {
-            template:
-                "<label>@</label> <input value='{{ at }}' />" +
-                "<label>=</label> <input ng-model='equals' />" +
-                "<label>&</label> <input type='button' ng-click='ampersand()' value='Btn' />",
-            scope: {
-                at:        "@",
-                equals:    "=",
-                ampersand: "&"
-            }
-        };
-    })
-    .directive("jaysDirective", function(){
-        return {
-            template:
-                "<label>@</label> <input value='{{at2}}' />" +
-                "<label>=</label> <input ng-model='equals2' />" +
-                "<label>&</label> <input type='button' value='Click me' ng-click='ampersand2()' />",
-            scope: {
-                at2:        "@",
-                equals2:    "=",
-                ampersand2: "&"
-            }
-        };
-    });
+	.directive("jaysDirective", function(){
+		return {
+			restrict: "E",
+			templateUrl: "inputs.html",
+			scope: {
+				at2:        "@",
+				equals2:    "=",
+				ampersand2: "&"
+			}
+		};
+	});
